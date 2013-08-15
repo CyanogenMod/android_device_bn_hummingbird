bf100b29b61300a4bd5e72fe898182ae7354f9ca
Hashcode <hashcode0f@gmail.com>
2013-08-15 16:13:36 -0700

    reverse build product order to fix prop set order
    Change-Id: I008b7598d82f7724fe3520cae7c2c40c3bb2580c

--- full_hummingbird.mk
+++ full_hummingbird.mk
@@ -21,10 +21,10 @@
 #
 
 # Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+$(call inherit-product, device/bn/hummingbird/device.mk)
 # This is where we'd set a backup provider if we had one
 #$(call inherit-product, device/sample/products/backup_overlay.mk)
-$(call inherit-product, device/bn/hummingbird/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
 # Discard inherited values and use our own instead.
 PRODUCT_NAME := full_hummingbird
